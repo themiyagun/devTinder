3e76b23c862f2d8d7b278b5bfd1ddf986185ec36
themiyagun <themiya.gunasekara95@gmail.com>
2026-04-23 16:41:47 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const app = express(); // create express js application
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "John", lastName: "Doe" });
-});
+// app.get("/user/:id", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firstName: "John", lastName: "Doe" });
+// });
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("first clg");
+    // res.send("response1");
+    next();
+  },
+  (req, res) => {
+    console.log("2nd clg");
+    res.send("2nd response");
+  },
+);
 
 app.post("/user", (req, res) => {
   res.send("send user data successfull to database");
